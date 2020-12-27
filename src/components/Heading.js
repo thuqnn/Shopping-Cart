a18c2a7a3b764be222e9c1f43ba51238a7ee12f1
thuqnn <phamthithuqn92@gmail.com>
2020-12-27 21:36:28 +0700

    splits components

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,15 +1,13 @@
 function Heading() {
   return (
-    <>
-      <header className="container">
-        <h1>Shopping Cart</h1>
-        <ul className="breadcrumb">
-          <li>Home</li>
-          <li>Shopping Cart</li>
-        </ul>
-        <span className="count">3 items in the bag</span>
-      </header>
-    </>
+    <header className="container">
+      <h1>Shopping Cart</h1>
+      <ul className="breadcrumb">
+        <li>Home</li>
+        <li>Shopping Cart</li>
+      </ul>
+      <span className="count">3 items in the bag</span>
+    </header>
   );
 }
 
