993167331b2a18b08e9cc82de5d8b4646b8de69d
thuqnn <phamthithuqn92@gmail.com>
2020-12-27 21:31:01 +0700

    split commponent

---
+++ src/components/Heading.js
@@ -0,0 +1,16 @@
+function Heading() {
+  return (
+    <>
+      <header className="container">
+        <h1>Shopping Cart</h1>
+        <ul className="breadcrumb">
+          <li>Home</li>
+          <li>Shopping Cart</li>
+        </ul>
+        <span className="count">3 items in the bag</span>
+      </header>
+    </>
+  );
+}
+
+export default Heading;
